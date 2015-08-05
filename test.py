9fc92e561cc9a9d961a82f58f65085a3e5d7fbf2
Jasper Hugunin <jasper@hugunin.net>
2015-08-05 11:04:04 -0700

    Update documentation based on feedback from Alex

--- test.py
+++ test.py
@@ -17,10 +17,17 @@ class TestChannel(unittest.TestCase):
             time.sleep(0.5)
 
     def setUp(self):
+        # Set up 3 nodes: Alice, Bob, and Carol
         proxies = create_regnet.main()
         self.alice, self.bob, self.carol = proxies
+        # self.alice.bit is an interface to bitcoind,
+        # self.alice.lit talks to the lightning node
+        # self.alice.lurl is Alice's identifier
+        # Carol will be the miner and generate all the blocks
+        # Generate 101 blocks so that Carol has funds
         self.carol.bit.generate(101)
         self.propagate()
+        # Give 1 BTC each to Alice and Bob
         self.carol.bit.sendmany(
             "",
             {self.alice.bit.getnewaddress(): 100000000,
@@ -38,22 +45,33 @@ class TestChannel(unittest.TestCase):
 
     def test_basic(self):
         """Test basic operation of a payment channel."""
+        # Open a channel between Alice and Bob
         self.alice.lit.create(self.bob.lurl, 50000000, 25000000)
         self.propagate()
+        # There are some fees associated with opening a channel
         afee = 50000000 - self.alice.bit.getbalance()
         bfee = 75000000 - self.bob.bit.getbalance()
         self.assertGreaterEqual(afee, 0)
         self.assertGreaterEqual(bfee, 0)
+        # (Balance) Alice: 0.50 BTC, Bob: 0.25 BTC
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 50000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 25000000)
+        # Bob sends Alice 0.05 BTC
         self.bob.lit.send(self.alice.lurl, 5000000)
+        # (Balance) Alice: 0.55 BTC, Bob: 0.20 BTC
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 55000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 20000000)
+        # Now Alice sends Bob 0.10 BTC
         self.alice.lit.send(self.bob.lurl, 10000000)
+        # (Balance) Alice: 0.45 BTC, Bob: 0.30 BTC
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 45000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 30000000)
+        # Bob closes the channel
         self.bob.lit.close(self.alice.lurl)
         self.propagate()
+        # The Lightning balance is returned to the bitcoin wallet
+        # If any coin was held for fees which were never paid,
+        # they are refunded, so the balance may be more than expected.
         self.assertGreaterEqual(self.alice.bit.getbalance(), 95000000 - afee)
         self.assertGreaterEqual(self.bob.bit.getbalance(), 105000000 - bfee)
 
@@ -129,6 +147,7 @@ class TestLightning(unittest.TestCase):
             time.sleep(0.5)
 
     def setUp(self):
+        # As in TestChannel, set up 3 nodes
         proxies = create_regnet.main()
         self.alice, self.bob, self.carol = proxies
         self.carol.bit.generate(101)
@@ -139,6 +158,7 @@ class TestLightning(unittest.TestCase):
              self.bob.bit.getnewaddress(): 100000000,
             })
         self.propagate()
+        # Set up channels between so the network is Alice - Carol - Bob
         self.alice.lit.create(self.carol.lurl, 50000000, 50000000)
         self.propagate()
         self.bob.lit.create(self.carol.lurl, 50000000, 50000000)
@@ -149,6 +169,9 @@ class TestLightning(unittest.TestCase):
 
     def test_setup(self):
         """Test that the setup worked."""
+        # (Balance) Alice-Carol: Alice: 0.50 BTC, Carol 0.50 BTC
+        #           Carol-Bob  : Carol: 0.50 BTC, Bob   0.50 BTC
+        # (Total) Alice: 0.50 BTC, Carol: 1.00 BTC, Bob: 0.50 BTC
         self.assertEqual(self.alice.lit.getbalance(self.carol.lurl), 50000000)
         self.assertEqual(self.bob.lit.getbalance(self.carol.lurl), 50000000)
         self.assertEqual(self.carol.lit.getbalance(self.alice.lurl), 50000000)
@@ -156,9 +179,15 @@ class TestLightning(unittest.TestCase):
 
     def test_payment(self):
         """Test multi-hop payment."""
+        # Note Alice and Bob do not have a payment channel open directly.
+        # They are connected through Carol
         self.alice.lit.send(self.bob.lurl, 5000000)
+        # There is a fee associated with multi-hop payments
         fee = 45000000 - self.alice.lit.getbalance(self.carol.lurl)
         self.assertGreaterEqual(fee, 0)
+        # (Balance) Alice-Carol: Alice: 0.45 - fee BTC, Carol 0.55 + fee BTC
+        #           Carol-Bob  : Carol: 0.45       BTC, Bob   0.55       BTC
+        # (Total) Alice: 0.45 - fee BTC, Carol: 1.00 + fee BTC, Bob: 0.55 BTC
         self.assertEqual(self.carol.lit.getbalance(self.alice.lurl), 55000000 + fee)
         self.assertEqual(self.bob.lit.getbalance(self.carol.lurl), 55000000)
         self.assertEqual(self.carol.lit.getbalance(self.bob.lurl), 45000000)
