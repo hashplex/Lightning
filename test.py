c26595c19bde486c46140593809dfb126b0defd8
Jasper Hugunin <jasper@hugunin.net>
2015-08-07 13:00:24 -0700

    Add documentation for tests

--- test.py
+++ test.py
@@ -40,6 +40,7 @@ class TestChannel(unittest.TestCase):
 
     def test_setup(self):
         """Test that the setup worked."""
+        # Alice and Bob each start with 1.00 BTC
         self.assertEqual(self.alice.bit.getbalance(), 100000000)
         self.assertEqual(self.bob.bit.getbalance(), 100000000)
 
@@ -77,85 +78,111 @@ class TestChannel(unittest.TestCase):
 
     def test_stress(self):
         """Test edge cases in payment channels."""
+        # Open *two* payment channels Bob - Alice - Carol
         self.alice.lit.create(self.bob.lurl, 25000000, 50000000)
         self.propagate()
         self.carol.lit.create(self.alice.lurl, 50000000, 25000000)
         self.propagate()
+        # Account for fees
         afee = 50000000 - self.alice.bit.getbalance()
         bfee = 50000000 - self.bob.bit.getbalance()
         self.assertGreaterEqual(afee, 0)
         self.assertGreaterEqual(bfee, 0)
+        # Balance (A-C) Alice: 0.25 BTC, Carol: 0.50 BTC
+        # Balance (B-A) Bob:   0.50 BTC, Alice: 0.25 BTC
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 25000000)
         self.assertEqual(self.alice.lit.getbalance(self.carol.lurl), 25000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 50000000)
         self.assertEqual(self.carol.lit.getbalance(self.alice.lurl), 50000000)
+        # Carol sends 0.25 BTC to Alice
         self.carol.lit.send(self.alice.lurl, 25000000)
+        # Balance (A-C) Alice: 0.50 BTC, Carol: 0.25 BTC
         self.assertEqual(self.alice.lit.getbalance(self.carol.lurl), 50000000)
         self.assertEqual(self.carol.lit.getbalance(self.alice.lurl), 25000000)
+        # Alice sends 0.15 BTC to Carol
         self.alice.lit.send(self.carol.lurl, 15000000)
+        # Balance (A-C) Alice: 0.35 BTC, Carol: 0.40 BTC
         self.assertEqual(self.alice.lit.getbalance(self.carol.lurl), 35000000)
         self.assertEqual(self.carol.lit.getbalance(self.alice.lurl), 40000000)
+        # Bob sends Alice 0.50 BTC (his whole balance)
         self.bob.lit.send(self.alice.lurl, 50000000)
+        # Balance (B-A) Bob:   0.00 BTC, Alice: 0.75 BTC
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 75000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 0)
+        # Alice sends Bob 0.75 BTC (her whole balance)
         self.alice.lit.send(self.bob.lurl, 75000000)
+        # Balance (B-A) Bob:   0.75 BTC, Alice: 0.00 BTC
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 0)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 75000000)
+        # Alice closes the channel with Bob, on an empty account (Alice opened)
         self.alice.lit.close(self.bob.lurl)
         self.propagate()
         self.assertGreaterEqual(self.alice.bit.getbalance(), 50000000 - afee)
         self.assertGreaterEqual(self.bob.bit.getbalance(), 125000000 - bfee)
+        # Alice closes the channel with Carol (Carol opened)
         self.alice.lit.close(self.carol.lurl)
         self.propagate()
         self.assertGreaterEqual(self.alice.bit.getbalance(), 85000000 - afee)
 
     def test_unilateral_close(self):
         """Test unilateral close."""
+        # Set up channel between Alice and Bob
         self.alice.lit.create(self.bob.lurl, 50000000, 25000000)
         self.propagate()
         afee = 50000000 - self.alice.bit.getbalance()
         bfee = 75000000 - self.bob.bit.getbalance()
         self.assertGreaterEqual(afee, 0)
         self.assertGreaterEqual(bfee, 0)
+        # Do some transactions
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 50000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 25000000)
         self.bob.lit.send(self.alice.lurl, 5000000)
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 55000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 20000000)
+        # Kill Bob
         stop(self.bob.lpid)
+        # Publish Alice's commitment transactions
         commitment = self.alice.lit.getcommitmenttransactions(self.bob.lurl)
         for transaction in commitment:
             self.alice.bit.sendrawtransaction(transaction)
             self.propagate()
         time.sleep(1)
         self.propagate()
+        # Alice and Bob get their money out
         self.assertGreaterEqual(self.bob.bit.getbalance(), 95000000 - bfee)
         self.assertGreaterEqual(self.alice.bit.getbalance(), 105000000 - afee)
 
     @unittest.expectedFailure
     def test_revoked(self):
         """Test a revoked commitment transaction being published."""
+        # Set up channel between Alice and Bob
         self.alice.lit.create(self.bob.lurl, 50000000, 25000000)
         self.propagate()
         afee = 50000000 - self.alice.bit.getbalance()
         bfee = 75000000 - self.bob.bit.getbalance()
         self.assertGreaterEqual(afee, 0)
         self.assertGreaterEqual(bfee, 0)
+        # Make a transaction
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 50000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 25000000)
         self.bob.lit.send(self.alice.lurl, 5000000)
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 55000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 20000000)
+        # Save Alice's old commitment transactions
         commitment = self.alice.lit.getcommitmenttransactions(self.bob.lurl)
+        # Do annother transaction, Alice sends Bob money
         self.alice.lit.send(self.bob.lurl, 10000000)
         self.assertEqual(self.alice.lit.getbalance(self.bob.lurl), 45000000)
         self.assertEqual(self.bob.lit.getbalance(self.alice.lurl), 30000000)
+        # Alice stops responding
         stop(self.alice.lpid)
+        # She publishes her old, revoked commitment transactions
         for transaction in commitment:
             self.alice.bit.sendrawtransaction(transaction)
             self.propagate()
         time.sleep(1)
         self.propagate()
+        # Bob ends up with all the money
         self.assertGreaterEqual(self.bob.bit.getbalance(), 150000000 - bfee)
 
 class TestLightning(unittest.TestCase):
